6a2a7659e5af4746a5f9f49d5cec390ae7b90aa6
win_boy <godwinokon112@gmail.com>
2025-02-05 02:58:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+// properties or variables
+const score = document.getElementById('score');
+const colorDisplay = document.getElementById('colorDisplay');
+const buttonContainer = document.querySelector('.color-container');
+const resetBtn = document.querySelector('.reset');
+const result = document.querySelector('#result');
+let count = 0;
+const colorArray = ['red', 'green', 'yellow', 'pink', 'blue', 'orange'];
+
+let random = Math.floor(Math.random() * colorArray.length + 1);
+let color = colorArray[random];
+let display = (colorDisplay.style.backgroundColor = color);
+
+function updateDisplay() {
+  score.innerText = count;
+}
+
+buttonContainer.addEventListener('click', (e) => {
+  if (e.target.id == 'green') {
+    if (colorDisplay.style.backgroundColor == 'green') {
+      score.innerText = count++;
+      result.innerText = 'correct!!';
+      result.style.color = 'green';
+      result.innerText = 'correct!!';
+      result.style.color = 'green';
+      updateDisplay();
+    } else {
+      result.innerText = 'wrong!!';
+      result.style.color = 'red';
+    }
+  } else if (e.target.id == 'red') {
+    if (colorDisplay.style.backgroundColor == 'red') {
+      score.innerText = count++;
+      result.innerText = 'correct!!';
+      result.style.color = 'green';
+      updateDisplay();
+    } else {
+      result.innerText = 'wrong!!';
+      result.style.color = 'red';
+    }
+  } else if (e.target.id == 'orange') {
+    if (colorDisplay.style.backgroundColor == 'orange') {
+      score.innerText = count++;
+      result.innerText = 'correct!!';
+      result.style.color = 'green';
+      updateDisplay();
+    } else {
+      result.innerText = 'wrong!!';
+      result.style.color = 'red';
+    }
+  } else if (e.target.id == 'blue') {
+    if (colorDisplay.style.backgroundColor == 'blue') {
+      score.innerText = count++;
+      result.innerText = 'correct!!';
+      result.style.color = 'green';
+      updateDisplay();
+    } else {
+      result.innerText = 'wrong!!';
+      result.style.color = 'red';
+    }
+  } else if (e.target.id == 'yellow') {
+    if (colorDisplay.style.backgroundColor == 'yellow') {
+      score.innerText = count++;
+      result.innerText = 'correct!!';
+      result.style.color = 'green';
+      updateDisplay();
+    } else {
+      result.innerText = 'wrong!!';
+      result.style.color = 'red';
+    }
+  } else if (e.target.id == 'pink') {
+    if (colorDisplay.style.backgroundColor == 'pink') {
+      score.innerText = count++;
+      result.innerText = 'correct!!';
+      result.style.color = 'green';
+      updateDisplay();
+    } else {
+      result.innerText = 'wrong!!';
+      result.style.color = 'red';
+    }
+  } else if (e.target.id == 'reset') {
+    count = 0;
+    updateDisplay();
+    result.style.color = 'navy';
+    result.innerText = 'Guess the color Again!';
+    score.innerText = 0;
+    let random = Math.floor(Math.random() * colorArray.length + 1);
+    let color = colorArray[random];
+    let display = (colorDisplay.style.backgroundColor = color);
+    return display;
+  } else {
+    return;
+  }
+});
